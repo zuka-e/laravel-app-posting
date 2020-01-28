ebfa245de29f2d269da9cb9ca293ecd8d8686c8a
zuka-e <nq.iizuka@gmail.com>
2020-01-28 23:04:45 +0900

    Fix bug that can't view root_url

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,11 +10,22 @@
   @endif
   <div class="col-lg-3 d-none d-lg-block">
     <div class="card mb-4">
+      @guest
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <a class="btn btn-primary btn-block" href="{{ route('login') }}">{{ __('ログイン') }}</a>
+          </li>
+          <li class="list-group-item">
+            <a class="btn btn-success btn-block" href="{{ route('register') }}">{{ __('新規登録') }}</a>
+          </li>
+        </ul>
+      @else
       <img src="/storage/users/{{ $user->image }}" onerror="this.src='/storage/no_image.jpg'" class="image card-img-top" width="100%" height="100%"/>
       <ul class="list-group list-group-flush">
         <li class="list-group-item">@ {{ $user->name }}</li>
         <li class="list-group-item">投稿 {{ count($user->posts) }}件</li>
       </ul>
+      @endguest
     </div>
   </div>
   <div class="col-lg-7">
